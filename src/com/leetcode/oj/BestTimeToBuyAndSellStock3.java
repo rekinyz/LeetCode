cf5b74dc1c09048c28acf8dc59dc81be6e0f2296
rekinyz <rekinyz@hotmail.com>
2014-12-05 18:00:35 +0100

    improve the solution of BestTimeToBuyAndSellStock3

--- src/com/leetcode/oj/BestTimeToBuyAndSellStock3.java
+++ src/com/leetcode/oj/BestTimeToBuyAndSellStock3.java
@@ -25,20 +25,12 @@ public class BestTimeToBuyAndSellStock3 {
 		int minIdx = 0;
 		int maxIdx = len-1;
 		if(len>1){
-			for(int i=1;i<len;i++){
-				if(prices[minIdx] > prices[i-1]){
-					minIdx = i-1;
-				}
+			for(int i=1,j=len-1;i<len;i++,j--){
+				if(prices[minIdx] > prices[i-1]) minIdx = i-1;
+				if(prices[maxIdx] < prices[j-1]) maxIdx = j-1;
 				left[i] = Math.max(left[i-1], prices[i-1]-prices[minIdx]);
+				right[j] = Math.max(right[j+1], prices[maxIdx]-prices[j-1]);
 			}
-			
-	        for(int i=len-1; i>0; i--){
-	        	if(prices[maxIdx] < prices[i-1]){
-	        		maxIdx = i-1;
-	        	}
-	        	right[i] = Math.max(right[i+1], prices[maxIdx]-prices[i-1]);
-	        }
-			
 			for(int i=0;i<len-1;i++){
 				profit = Math.max(profit, right[i+1]+left[i]);
 			}
