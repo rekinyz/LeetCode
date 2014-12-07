8256cfb1f9847449a35dd505ed4c34698928cfc2
rekinyz <rekinyz@hotmail.com>
2014-12-07 12:31:49 +0100

    improve the test of "Maximum Product Subarray"

--- test/com/leetcode/oj/MaximumProductSubarrayTest.java
+++ test/com/leetcode/oj/MaximumProductSubarrayTest.java
@@ -11,37 +11,37 @@ public class MaximumProductSubarrayTest {
 	@Test
 	public void testMaxProduct0() {
 		int[] num = { 2, 3, -2, 4 };
-		assertEquals(ps.maxProduct(num), 6);
+		assertEquals(maxProduct(num), 6);
 	}
 
 	@Test
 	public void testMaxProduct1() {
 		int[] num = { 2, 0 };
-		assertEquals(ps.maxProduct(num), 2);
+		assertEquals(maxProduct(num), 2);
 	}
 
 	@Test
 	public void testMaxProduct2() {
 		int[] num = { -2 };
-		assertEquals(ps.maxProduct(num), -2);
+		assertEquals(maxProduct(num), -2);
 	}
 
 	@Test
 	public void testMaxProduct3() {
 		int[] num = { -2, 3, -4 };
-		assertEquals(ps.maxProduct(num), 24);
+		assertEquals(maxProduct(num), 24);
 	}
 
 	@Test
 	public void testMaxProduct4() {
 		int[] num = { 2, -5, -2, -4, 3 };
-		assertEquals(ps.maxProduct(num), 24);
+		assertEquals(maxProduct(num), 24);
 	}
 
 	@Test
 	public void testMaxProduct5() {
 		int[] num = { 3, -1, 4 };
-		assertEquals(ps.maxProduct(num), 4);
+		assertEquals(maxProduct(num), 4);
 	}
 
 	@Test
@@ -654,42 +654,46 @@ public class MaximumProductSubarrayTest {
 				1, -1, -1, 1, -1, 1, 1, -1, -1, -1, -1, 1, 1, -1, -1, 1, -1, 1,
 				-1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, 1, 1, 1, -1, 1, -1,
 				1, 1, 1, 1, -1, -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, -1 };
-		assertEquals(ps.maxProduct(num), 1492992000);
+		assertEquals(maxProduct(num), 1492992000);
 	}
 
 	@Test
 	public void testMaxProduct7() {
 		int[] num = { -2, 0, -1 };
-		assertEquals(ps.maxProduct(num), 0);
+		assertEquals(maxProduct(num), 0);
 	}
 
 	@Test
 	public void testMaxProduct8() {
 		int[] num = { -5, 0, 2 };
-		assertEquals(ps.maxProduct(num), 2);
+		assertEquals(maxProduct(num), 2);
 	}
 
 	@Test
 	public void testMaxProduct9() {
 		int[] num = { 6, 3, -10, 0, 2 };
-		assertEquals(ps.maxProduct(num), 18);
+		assertEquals(maxProduct(num), 18);
 	}
 
 	@Test
 	public void testMaxProduct10() {
 		int[] num = { 1, -2, 3, -4, -3, -4, -3 };
-		assertEquals(ps.maxProduct(num), 432);
+		assertEquals(maxProduct(num), 432);
 	}
 
 	@Test
 	public void testMaxProduct11() {
 		int[] num = { -3, 0, 1, -2 };
-		assertEquals(ps.maxProduct(num), 1);
+		assertEquals(maxProduct(num), 1);
 	}
 
 	@Test
 	public void testMaxProduct12() {
 		int[] num = { -1, -2, -3, 0 };
-		assertEquals(ps.maxProduct(num), 6);
+		assertEquals(maxProduct(num), 6);
+	}
+	
+	private int maxProduct(int[] array){
+		return ps.maxProduct(array);
 	}
 }
