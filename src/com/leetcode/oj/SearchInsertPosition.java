33cd59b246794d1b099ea634934b27237564828c
rekinyz <rekinyz@hotmail.com>
2014-12-12 15:42:58 +0100

    improved comment

--- src/com/leetcode/oj/SearchInsertPosition.java
+++ src/com/leetcode/oj/SearchInsertPosition.java
@@ -5,7 +5,10 @@ package com.leetcode.oj;
  * found. If not, return the index where it would be if it were inserted in
  * order. You may assume no duplicates in the array. Here are few examples.
  * 
- * [1,3,5,6], 5 → 2 [1,3,5,6], 2 → 1 [1,3,5,6], 7 → 4 [1,3,5,6], 0 → 0
+ * [1,3,5,6], 5 → 2 
+ * [1,3,5,6], 2 → 1 
+ * [1,3,5,6], 7 → 4 
+ * [1,3,5,6], 0 → 0
  * 
  * @author rekinyz
  */
--- src/com/leetcode/oj/ValidNumber.java
+++ src/com/leetcode/oj/ValidNumber.java
@@ -5,7 +5,11 @@ package com.leetcode.oj;
  * 
  * Validate if a given string is numeric.
  * 
- * Some examples: "0" => true " 0.1 " => true "abc" => false "1 a" => false
+ * Some examples: 
+ * "0" => true 
+ * "0.1 " => true 
+ * "abc" => false 
+ * "1 a" => false
  * "2e10" => true
  * 
  * Note: It is intended for the problem statement to be ambiguous. You should
