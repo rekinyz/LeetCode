893faf8ad23538680eaf872e156352ed7e39b168
rekinyz <rekinyz@hotmail.com>
2014-12-15 21:10:48 +0100

    improve the unit test for the "LinkedList Cycle"

--- test/com/leetcode/oj/LinkedListCycleTest.java
+++ test/com/leetcode/oj/LinkedListCycleTest.java
@@ -9,7 +9,7 @@ import com.leetcode.oj.util.ListNode;
 
 public class LinkedListCycleTest {
 
-	LinkedListCycle list = new LinkedListCycle();
+	LinkedListCycle linkedList = new LinkedListCycle();
 
 	@Test
 	public void testHasCycle0() {
@@ -32,28 +32,29 @@ public class LinkedListCycleTest {
 	@Test
 	public void testHasCycle3() {
 		int[] input = { 0, 1, 2, 4, 5, 6, 7 };
-		assertTrue(hasCircleCycle(input));
+		assertTrue(hasCycle(input, 2));
 	}
 
 	@Test
 	public void testHasCycle4() {
 		int[] input = { 2, 3, 4, 4, 5 };
-		assertTrue(hasCircleCycle(input));
+		assertTrue(hasCycle(input, 3));
 	}
 
 	@Test
 	public void testHasCycle5() {
 		int[] input = { 3, 2, 0, -4 };
-		assertTrue(hasCircleCycle(input));
+		assertTrue(hasCycle(input, 4));
 	}
 
 	private boolean hasCycle(int[] input) {
-		ListNode node = ListNode.convert(input);
-		return list.hasCycle(node);
+		ListNode list = ListNode.convert(input);
+		return linkedList.hasCycle(list);
 	}
 
-	private boolean hasCircleCycle(int[] input) {
-		ListNode node = ListNode.circle(input);
-		return list.hasCycle(node);
+	private boolean hasCycle(int[] input, int n) {
+		ListNode list = ListNode.cycle(input, n);
+		return linkedList.hasCycle(list);
 	}
+	
 }
