87a08351a84286336ffec7b8612b593a8f0c6431
rekinyz <rekinyz@hotmail.com>
2014-12-17 18:33:28 +0100

    tiny improvements and corrections for all test cases

--- test/com/leetcode/oj/FindMinimumInRotatedSortedArray2Test.java
+++ test/com/leetcode/oj/FindMinimumInRotatedSortedArray2Test.java
@@ -11,30 +11,28 @@ public class FindMinimumInRotatedSortedArray2Test {
 	@Test
 	public void testFindMin1() {
 		int[] s = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1, 2, 3 };
-		assertEquals(sa2.findMin(s), 1);
+		assertEquals(1, findMin(s));
 	}
 
 	@Test
 	public void testFindMin2() {
 		int[] s = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-		assertEquals(sa2.findMin(s), 1);
+		assertEquals(1, findMin(s));
 	}
 
 	@Test
 	public void testFindMin3() {
 		int[] s = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1 };
-		assertEquals(sa2.findMin(s), 1);
+		assertEquals(1, findMin(s));
 	}
 
 	@Test
 	public void testFindMin4() {
 		int[] s = { 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6 };
-		assertEquals(sa2.findMin(s), 1);
+		assertEquals(1, findMin(s));
 	}
 
-	@Test
-	public void testFindMin5() {
-		int[] s = { 7, 8, 9, 10, 11, 12, 13, 14, 15, 15, 15, 16, 17, 18, 19, 20, 1, 2, 3, 4, 5, 6 };
-		assertEquals(sa2.findMin1(s), 1);
+	private int findMin(int[] num){
+		return sa2.findMin(num);
 	}
 }
