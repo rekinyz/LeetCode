418ec46e2e4697697545a051cbea37c8b6502169
rekinyz <rekinyz@hotmail.com>
2014-12-17 17:09:18 +0100

    add unit test for the "Add Binary"

---
+++ test/com/leetcode/oj/AddBinaryTest.java
@@ -0,0 +1,42 @@
+package com.leetcode.oj;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AddBinaryTest {
+
+	AddBinary ab = new AddBinary();
+
+	@Test
+	public void testAddBinary1() {
+		String a = "11";
+		String b = "1";
+		assertEquals(addBinary(a, b), "100");
+	}
+
+	@Test
+	public void testAddBinary2() {
+		String a = "11";
+		String b = "110";
+		assertEquals(addBinary(a, b), "1001");
+	}
+	
+	@Test
+	public void testAddBinary3() {
+		String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+		String b = "1";
+		assertEquals(addBinary(a, b), "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001110");
+	}
+	
+	@Test
+	public void testAddBinary4() {
+		String a = "1111";
+		String b = "1111";
+		assertEquals(addBinary(a, b), "11110");
+	}
+	
+	private String addBinary(String a, String b) {
+		return ab.addBinary(a, b);
+	}
+}
