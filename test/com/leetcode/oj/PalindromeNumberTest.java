00610df7c7c7de18014db8e095a977d521832a35
rekinyz <rekinyz@hotmail.com>
2015-01-12 15:52:08 +0100

    add unit test for the "Palindrome Number"

---
+++ test/com/leetcode/oj/PalindromeNumberTest.java
@@ -0,0 +1,52 @@
+package com.leetcode.oj;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PalindromeNumberTest {
+
+	PalindromeNumber pn = new PalindromeNumber();
+
+	@Test
+	public void testIsPalindrome1() {
+		int x = 909;
+		assertTrue(isPalindrome(x));
+	}
+
+	@Test
+	public void testIsPalindrome2() {
+		int x = 123454321;
+		assertTrue(isPalindrome(x));
+	}
+
+	@Test
+	public void testIsPalindrome3() {
+		int x = 90;
+		assertFalse(isPalindrome(x));
+	}
+
+	@Test
+	public void testIsPalindrome4() {
+		int x = -2147447412;
+		assertFalse(isPalindrome(x));
+	}
+
+	@Test
+	public void testIsPalindrome5() {
+		int x = -1;
+		assertFalse(isPalindrome(x));
+	}
+
+	@Test
+	public void testIsPalindrome6() {
+		int x = 0;
+		assertTrue(isPalindrome(x));
+	}
+
+	private boolean isPalindrome(int x) {
+		return pn.isPalindrome(x);
+	}
+
+}
