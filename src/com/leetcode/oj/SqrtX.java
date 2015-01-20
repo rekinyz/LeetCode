ca4adad555b4ccdd67dc20f8d4a92863ef17f521
rekinyz <rekinyz@hotmail.com>
2015-01-20 16:56:45 +0100

    clean up

--- src/com/leetcode/oj/SqrtX.java
+++ src/com/leetcode/oj/SqrtX.java
@@ -37,16 +37,4 @@ public class SqrtX {
 		return (int) res;
 	}
 
-	// newton's method for double type root
-	double sqrtNewton(double x) {
-		if (x == 0)
-			return 0;
-		double last = 0.0;
-		double res = 1.0;
-		while (res != last) {
-			last = res;
-			res = (res + x / res) / 2;
-		}
-		return res;
-	}
 }
