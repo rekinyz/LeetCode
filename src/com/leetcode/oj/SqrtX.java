052edfbb9ab90b92753467228b35561004172342
rekinyz <rekinyz@hotmail.com>
2015-01-20 16:31:49 +0100

    add newton's method for the "Sqrt()"

--- src/com/leetcode/oj/SqrtX.java
+++ src/com/leetcode/oj/SqrtX.java
@@ -24,4 +24,28 @@ public class SqrtX {
 		return (int) mid;
 	}
 
+	//newton's algorithm
+	public int sqrt1(int x) {
+	    if (x == 0) return 0;
+	    double last = 0;
+	    double res = 1;
+	    while (res != last) {
+	        last = res;
+	        res = (res + x / res) / 2;
+	    }
+	    return (int) res;
+	}
+	
+	//newton's method for double type root
+	double sqrtNewton(double x) {
+	    if (x == 0) return 0;
+	    double last = 0.0;
+	    double res = 1.0;
+	    while (res != last)
+	    {
+	        last = res;
+	        res = (res + x / res) / 2;
+	    }
+	    return res;
+	}
 }
