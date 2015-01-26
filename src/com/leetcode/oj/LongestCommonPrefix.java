eeffeacf87128d9c9ba6deb626b4d3d01b41a95c
rekinyz <rekinyz@hotmail.com>
2015-01-26 18:23:06 +0100

    improve solution for the "Longest Common Prefix"

--- src/com/leetcode/oj/LongestCommonPrefix.java
+++ src/com/leetcode/oj/LongestCommonPrefix.java
@@ -16,7 +16,7 @@ public class LongestCommonPrefix {
 		}
 		String pre = strs[0];
 		for (int i = 1; i < strs.length; i++) {
-			while (strs[i].indexOf(pre) != 0)
+			while (!strs[i].startsWith(pre))
 				pre = pre.substring(0, pre.length() - 1);
 		}
 		return pre;
