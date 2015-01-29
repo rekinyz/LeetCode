fa103b01448e0b4d199b4f068a968a171a4669f2
rekinyz <rekinyz@hotmail.com>
2015-01-29 21:50:35 +0100

    add unit test for the "Valid Parentheses"

---
+++ test/com/leetcode/oj/ValidParenthesesTest.java
@@ -0,0 +1,46 @@
+package com.leetcode.oj;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class ValidParenthesesTest {
+
+	ValidParentheses vp = new ValidParentheses();
+
+	@Test
+	public void testIsValid1() {
+		String s = "({})[()]{[]}";
+		assertTrue(isValid(s));
+	}
+
+	@Test
+	public void testIsValid2() {
+		String s = "([)]";
+		assertFalse(isValid(s));
+	}
+	
+	@Test
+	public void testIsValid3() {
+		String s = "([{}])";
+		assertTrue(isValid(s));
+	}
+	
+	@Test
+	public void testIsValid4() {
+		String s = "([{}]()";
+		assertFalse(isValid(s));
+	}
+	
+	@Test
+	public void testIsValid5() {
+		String s = "[]{}()";
+		assertTrue(isValid(s));
+	}
+
+	private boolean isValid(String s) {
+		return vp.isValid(s);
+	}
+
+}
