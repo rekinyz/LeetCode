8b62e4f851fe4fe27bc3d030490618766ea2863d
rekinyz <rekinyz@hotmail.com>
2015-01-31 23:14:32 +0100

    add bad O(N+NlogN) time solution to the "Single Number" as comparison

--- src/com/leetcode/oj/SingleNumber.java
+++ src/com/leetcode/oj/SingleNumber.java
@@ -1,7 +1,9 @@
 package com.leetcode.oj;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 /**
  * Single Number
@@ -16,6 +18,7 @@ import java.util.Set;
  */
 public class SingleNumber {
 
+	//O(N) time, O(1) space
 	public int singleNumber(int[] A) {
 		int result = A[0];
 		for (int i = 1; i < A.length; i++) {
@@ -24,6 +27,7 @@ public class SingleNumber {
 		return result;
 	}
 
+	//O(N) time,  O(N) space
 	public int singleNumber1(int[] A) {
 		Set<Integer> set = new HashSet<Integer>();
 		for (int a : A) {
@@ -35,4 +39,19 @@ public class SingleNumber {
 		}
 		return set.iterator().next();
 	}
+	
+	//O(N+NlogN) time, O(N+1) space
+	public int singleNumber2(int[] A) {
+		Arrays.sort(A);
+		Stack<Integer> stack = new Stack<>();
+		for (int a : A) {
+			if(stack.isEmpty()||stack.peek()!=a){
+				stack.push(a);
+			}else{
+				stack.pop();
+			}
+		}
+		return stack.pop();
+	}
+	
 }
