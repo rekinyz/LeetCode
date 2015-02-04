14f38afce42eb4d79b09602824ad3e017fd17a70
rekinyz <rekinyz@hotmail.com>
2015-02-04 23:27:32 +0100

    add unit test for the "Implement strStr()"

---
+++ test/com/leetcode/oj/ImplementStrStrTest.java
@@ -0,0 +1,78 @@
+package com.leetcode.oj;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ImplementStrStrTest {
+
+	ImplementStrStr iss = new ImplementStrStr();
+
+	@Test
+	public void testStrStr1() {
+		String haystack = "abcdefghijklmnopqrstuvwxyz";
+		String needle = "ijk";
+		assertEquals(8, strStr(haystack, needle));
+	}
+
+	@Test
+	public void testStrStr2() {
+		String haystack = "abcdefghijklmnopqrstuvwxyz";
+		String needle = "abc";
+		assertEquals(0, strStr(haystack, needle));
+	}
+	
+	@Test
+	public void testStrStr3() {
+		String haystack = "abcdefghijklmnopqrstuvwxyz";
+		String needle = "stuvwxyz";
+		assertEquals(18, strStr(haystack, needle));
+	}
+	
+	@Test
+	public void testStrStr4() {
+		String haystack = "abcdefgh";
+		String needle = "ijk";
+		assertEquals(-1, strStr(haystack, needle));
+	}
+
+	@Test
+	public void testStrStr5() {
+		String haystack = "defghijkabclmnopqrstuabcvwxyz";
+		String needle = "abc";
+		assertEquals(8, strStr(haystack, needle));
+	}
+	
+	@Test
+	public void testStrStr6() {
+		String haystack = "adsf";
+		String needle = "";
+		assertEquals(0, strStr(haystack, needle));
+	}
+	
+	@Test
+	public void testStrStr7() {
+		String haystack = "";
+		String needle = "";
+		assertEquals(0, strStr(haystack, needle));
+	}
+	
+	@Test
+	public void testStrStr8() {
+		String haystack = "";
+		String needle = "a";
+		assertEquals(-1, strStr(haystack, needle));
+	}
+	
+	@Test
+	public void testStrStr9() {
+		String haystack = "aaa";
+		String needle = "aaaa";
+		assertEquals(-1, strStr(haystack, needle));
+	}
+	
+	private int strStr(String haystack, String needle) {
+		return iss.strStr(haystack, needle);
+	}
+	
+}
