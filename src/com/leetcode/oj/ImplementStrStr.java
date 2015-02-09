f5a06586bbf78af494d05e2e21bd579559094f14
rekinyz <rekinyz@hotmail.com>
2015-02-09 21:11:36 +0100

    remove the solution that using indexOf(), it's cheating.

--- src/com/leetcode/oj/ImplementStrStr.java
+++ src/com/leetcode/oj/ImplementStrStr.java
@@ -9,24 +9,19 @@ package com.leetcode.oj;
  * @author rekinyz
  */
 public class ImplementStrStr {
-
-	public int strStr(String haystack, String needle) {
-		int res = -1;
-		while(haystack.contains(needle) && !haystack.startsWith(needle, ++res)){}
-		return res;
-	}
 	
-	public int strStr1(String haystack, String needle) {
-		int res = needle.isEmpty() ? 0 : haystack.indexOf(needle.charAt(0));
-		int i = 0, j = res, nLen = needle.length(), hLen = haystack.length();
-		while (res != -1 && i < nLen - 1 && hLen >= nLen + res) {
-			if (haystack.charAt(++j) != needle.charAt(++i)) {
-				i = 0;
-				res = haystack.indexOf(needle.charAt(i), ++res);
-				j = res;
-			}
-		}
-		return hLen >= nLen + res ? res : -1;
-	}
+	public int strStr(String haystack, String needle) {
+        int hLen = haystack.length(), nLen = needle.length();
+        for(int i = 0; i <= hLen-nLen; i++){
+            int j = 0;
+            for(; j<nLen; j++){
+                if(needle.charAt(j)!=haystack.charAt(i+j))
+                    break;
+            }
+            if(j == nLen)
+                return i;
+        }
+        return -1;
+    }
 
 }
