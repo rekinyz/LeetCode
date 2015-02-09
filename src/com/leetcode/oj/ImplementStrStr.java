28e03f4673b83c8ce722e06ae800de373c8847b5
rekinyz <rekinyz@hotmail.com>
2015-02-09 20:34:57 +0100

    add shortest & interesting solution to the "Implement strStr()"

--- src/com/leetcode/oj/ImplementStrStr.java
+++ src/com/leetcode/oj/ImplementStrStr.java
@@ -11,6 +11,12 @@ package com.leetcode.oj;
 public class ImplementStrStr {
 
 	public int strStr(String haystack, String needle) {
+		int res = -1;
+		while(haystack.contains(needle) && !haystack.startsWith(needle, ++res)){}
+		return res;
+	}
+	
+	public int strStr1(String haystack, String needle) {
 		int res = needle.isEmpty() ? 0 : haystack.indexOf(needle.charAt(0));
 		int i = 0, j = res, nLen = needle.length(), hLen = haystack.length();
 		while (res != -1 && i < nLen - 1 && hLen >= nLen + res) {
