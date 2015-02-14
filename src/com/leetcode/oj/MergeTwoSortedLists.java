6da32af99889d42ae41fa744298fb61519ff0029
rekinyz <rekinyz@hotmail.com>
2015-02-14 17:10:50 +0100

    add another shorter solution to the "Merge Two Sorted Lists"

--- src/com/leetcode/oj/MergeTwoSortedLists.java
+++ src/com/leetcode/oj/MergeTwoSortedLists.java
@@ -14,6 +14,25 @@ import com.leetcode.oj.util.ListNode;
 public class MergeTwoSortedLists {
 
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null) {
+			return l2;
+		}
+		if (l2 == null) {
+			return l1;
+		}
+
+		ListNode mergeHead;
+		if (l1.val < l2.val) {
+			mergeHead = l1;
+			mergeHead.next = mergeTwoLists(l1.next, l2);
+		} else {
+			mergeHead = l2;
+			mergeHead.next = mergeTwoLists(l1, l2.next);
+		}
+		return mergeHead;
+	}
+
+	public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
 		if (l1 == null)
 			return l2;
 		if (l2 == null)
@@ -49,5 +68,5 @@ public class MergeTwoSortedLists {
 
 		return head;
 	}
-
+	
 }
