3eb1db5c5fbee102d2ac4fd4dba327a2be3c30ad
rekinyz <rekinyz@hotmail.com>
2015-02-14 17:34:02 +0100

    override hashCode() and equals() for the ListNode Class

--- src/com/leetcode/oj/util/ListNode.java
+++ src/com/leetcode/oj/util/ListNode.java
@@ -65,6 +65,34 @@ public class ListNode {
 		}
 		return result.substring(0, result.length() - 2);
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((next == null) ? 0 : next.hashCode());
+		result = prime * result + val;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ListNode other = (ListNode) obj;
+		if (next == null) {
+			if (other.next != null)
+				return false;
+		} else if (!next.equals(other.next))
+			return false;
+		if (val != other.val)
+			return false;
+		return true;
+	}
 
 	public static void main(String[] s) {
 		int[] input0 = { 0, 1, 1, 2, 2, 3, 3 };
