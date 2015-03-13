5ad4f3f75bd86e6047b8b9496741a735d91d20eb
Julian Qian <julian@onlycoin.com>
2015-03-13 13:38:28 -0700

    über palindrome

---
+++ users/uber/Palindrome.java
@@ -0,0 +1,34 @@
+package uber;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class Palindrome {
+    // given string, can we use it to generate palindrome
+    // e.g. 'uber' --> False, 'aab' --> True, 'carecra' --> True
+    // assume no capital and no number
+    public static boolean canBePalindrome(String s) {
+        final int ANUM = 26;
+        boolean[] odd = new boolean[ANUM];
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            c -= 'a';
+            odd[c] = !odd[c];
+        }
+        int numOfOdd = 0;
+        for (int i = 0; i < ANUM; ++i) {
+            if (odd[i]) {
+                numOfOdd++;
+            }
+        }
+        
+        return numOfOdd < 1;
+    }
+    @Test
+    public void test() {
+        Assert.assertEquals(false, canBePalindrome("uber"));
+        Assert.assertEquals(false, canBePalindrome("carecra"));
+    }
+
+}
