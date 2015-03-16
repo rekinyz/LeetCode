60d4b859a9fdf33eb6708428cfdd85680b9a3959
Michael Gibson <michael@coin.vc>
2015-03-15 21:52:47 -0700

    Fibonacci.java

---
+++ users/uber/Fibonacci.java
@@ -0,0 +1,16 @@
+package uber;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Fibonacci {
+    // A encode to 1,B encode to 2,Z encode to 26,
+    // 32 can be decode to CB, 26 can be decode to Z or BF
+    // given a string of number, return how many ways to decode.
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
