b3284d6ea4fcb2333478e6305af9fae256d374f9
Michael Gibson <michael@coin.vc>
2015-03-16 21:58:29 -0700

    NextPermutation

--- users/uber/NextPermutation.java
+++ users/uber/NextPermutation.java
@@ -1,14 +1,17 @@
 package uber;
 
-import static org.junit.Assert.*;
+import junit.framework.Assert;
 
-import org.junit.Test;
-
+// leetcode 2.1.12, page 25
 public class NextPermutation {
-
+    public static String next(String n) {
+        
+    }
     @Test
     public void test() {
-        fail("Not yet implemented");
+        Assert.assertEquals("132", next("123"));
+        Assert.assertEquals("124", next("321"));
+        Assert.assertEquals("151", next("115"));
     }
 
 }
