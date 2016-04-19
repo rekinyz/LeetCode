13c14765e1abb175fe0dcb86547b6b4ea4f9d0bf
rekinyz <rekinyz@hotmail.com>
2016-04-19 22:42:25 +0200

    Divide Two Integers passed all unit tests, but the performance for the worse cases are bad.

--- src/com/leetcode/oj/DivideTwoIntegers.java
+++ src/com/leetcode/oj/DivideTwoIntegers.java
@@ -12,16 +12,21 @@ public class DivideTwoIntegers {
 	public int divide(final int dividend, final int divisor) {
 		int res = Integer.MAX_VALUE;
 		if (divisor != 0) {
-			res = (int) div(dividend, divisor, 0);
-			if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
+			res = (int) div(abs(dividend), abs(divisor));
+			if (dividend > 0 ^ divisor > 0) {
 				res = 0 - res;
 			}
 		}
 		return res;
 	}
 
-	private long div(final long x, final long y, final long total) {
-		return abs(x) < abs(y) ? total : div(abs(x) - abs(y), y, total + 1);
+	private long div(long x, final long y) {
+		long total = 0;
+		while(x >= y ){
+			x -= y;
+			total++;
+		}
+		return total;
 	}
 
 	private long abs(final long x) {
